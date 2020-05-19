1f37b5cdc4dd27f1853cf952613e6f34dec95a82
langheran <langheran@gmail.com>
2020-05-18 19:42:28 -0500

    changes

--- Matlab/data/samplegausian.m
+++ Matlab/data/samplegausian.m
@@ -1,4 +1,21 @@
+% x = samplegausian(N, mu, sigma) uses a mean mu and a standard deviation sigma
+% to generate a gaussian distribution in X.
+%
+% Inputs:
+% N         number of instances.       
+% mu        mean of the distribution.             
+% sigma     standard deviation of the distribution.             
+%
+% Outputs:
+% x         sample from the distribution.    (N x 1 vector with floating point numbers between 0 and 1 ).
+%
+% Copyright (C) Daphne Koller, Stanford Univerity, 2012
 function x = samplegausian(N, mu, sigma)
-% x = samplegausian(N, mu, sigma)
+% 
+mu = mu(:);
+R = chol(sigma);
+for i = 1:N
+    x(i,:) = mu' + (R'*randn(size(mu)))';
+end
 
 end
